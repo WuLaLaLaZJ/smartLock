e98340b65e22589c041dbe7be70982e4ba13950a
Scaxlibur <scaxlibur@gmail.com>
2024-10-21 20:25:58 +0800

    指纹识别开了个头

---
+++ main/fingerID.hpp
@@ -0,0 +1,23 @@
+#ifndef _fingerID_HPP
+#define _fingerID_HPP
+#pragma once
+
+#include <stdint.h>
+#include "UARTtrans.hpp"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+
+
+class IDENTIFIER
+{
+    private:
+
+    public:
+
+
+    IDENTIFIER();
+    ~IDENTIFIER();
+};
+
+#endif

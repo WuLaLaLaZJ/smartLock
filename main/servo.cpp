37f3da181620af3174ed7730b7974d8a2a998147
Scaxlibur <scaxlibur@gmail.com>
2024-10-23 21:05:59 +0800

    优化布局

--- main/servo.cpp
+++ main/servo.cpp
@@ -29,37 +29,35 @@ SERVO::~SERVO()
 bool SERVO::servo_init()
 {
     init_uart2servo();
-
-    char servoCommand[16];
-    strConnect(servoCommand, "#", servoID, "PID!");                                                 // 获取舵机ID的指令
-    ESP_ERROR_CHECK(uart_write_bytes(UART_NUM_SERVO, servoCommand, strlen(servoCommand)));          // 发送，然后读取串口
+    strConnect(servoCommand, "#", servoID, "PID!");                                        // 获取舵机ID的指令
+    ESP_ERROR_CHECK(uart_write_bytes(UART_NUM_SERVO, servoCommand, strlen(servoCommand))); // 发送，然后读取串口
     char UARTdata[64];
     servoUARTread(UARTdata);
-    char servoRetrun[16];
-    strConnect(servoRetrun, "#", servoID, "!");                                                     // 拼凑出舵机“应该”有的返回值
-    char *isOK = NULL;                                                                              // 串口读到的信息里有“应该”有的返回值吗？
+    strConnect(servoRetrun, "#", servoID, "!"); // 拼凑出舵机“应该”有的返回值
+    char *isOK = NULL;                          // 串口读到的信息里有“应该”有的返回值吗？
     isOK = strstr(UARTdata, servoRetrun);
     if (isOK != NULL)
     {
         isOK = NULL;
         strcpy(UARTdata, "\0");
         strcpy(servoRetrun, "\0");
-        strConnect(servoCommand, "#", servoID, "PMOD!");                                            // 舵机读取工作模式指令
+        strConnect(servoCommand, "#", servoID, "PMOD!"); // 舵机读取工作模式指令
         ESP_ERROR_CHECK(uart_write_bytes(UART_NUM_SERVO, servoCommand, strlen(servoCommand)));
         servoUARTread(UARTdata);
-        strConnect(servoRetrun, "#", servoID, "PMOD1!");                                            // 舵机的工作模式应该是舵机模式，顺时针最大270度，即1
+        strConnect(servoRetrun, "#", servoID, "PMOD1!"); // 舵机的工作模式应该是舵机模式，顺时针最大270度，即1
         isOK = strstr(UARTdata, servoRetrun);
         if (isOK != NULL)
         {
-            strcpy(UARTdata, "\0");                                                                 // 工作模式不是1就设置成1
+            strcpy(UARTdata, "\0"); // 工作模式不是1就设置成1
             strConnect(servoCommand, "#", servoID, "PMOD1!");
             ESP_ERROR_CHECK(uart_write_bytes(UART_NUM_SERVO, servoCommand, strlen(servoCommand)));
         }
         return true;
-    }else{
+    }
+    else
+    {
         return false;
     }
-    
 }
 
 /*******************************************************************************
@@ -69,11 +67,11 @@ bool SERVO::servo_init()
 ****函数备注: 初始位为500，开门位为2000
 ********************************************************************************/
 
-bool SERVO::opendoor(char resetPos[5] = "0500", char openPos[5] = "2000")
+bool SERVO::opendoor()
 {
-    char servoCommand[16];
-    strConnect(servoCommand, "#", servoID, "P", openPos, "T1000!");                                  //#000P2000T1000!
+    strConnect(servoCommand, "#", servoID, "P", openPos, "T1000!"); // #000P2000T1000!
     uart_write_bytes(UART_NUM_SERVO, servoCommand, strlen(servoCommand));
     vTaskDelay(1000);
     strConnect(servoCommand, "#", servoID, "P", resetPos, "T1000!");
+    uart_write_bytes(UART_NUM_SERVO, servoCommand, strlen(servoCommand));
 }
--- main/servo.hpp
+++ main/servo.hpp
@@ -9,14 +9,17 @@ class SERVO
 {
 private:
     char servoID[4] = "000";                                                //舵机编号，默认为000
-    
+    char servoCommand[16];                                                  //待发送的舵机指令
+    char servoRetrun[16];                                                   //应当接收到的舵机回传
+    char resetPos[5] = "0500";
+    char openPos[5] = "2000";
+
 public:
     bool servo_init(void);
-    bool opendoor(char resetPos[5] = "0500", char openPos[5] = "2000");
+    bool opendoor();
     void setServoID(char out_servoID);
     SERVO(char *out_servoID);                                               //构造函数
     ~SERVO();                                                               //析构函数
-    
 };
 
 #endif

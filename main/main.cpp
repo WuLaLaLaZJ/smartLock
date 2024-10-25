55b2ce4ddf1a6243d30a203be0f7e664666d7ec1
Scaxlibur <scaxlibur@gmail.com>
2024-10-25 21:22:32 +0800

    串口测试

--- main/main.cpp
+++ main/main.cpp
@@ -6,6 +6,8 @@ extern "C" void app_main(void)
 {
     printf("开机了");
     init_uart2id();
-    ESP_ERROR_CHECK(uart_write_bytes(UART_NUM_ID, "你好你好你好\n\n", sizeof("你好你好你好\n\n")));//这里能否运行是个问号
+    while(1){
+    ESP_ERROR_CHECK(uart_write_bytes(UART_NUM_ID, "你好你好你好\n\n", strlen("你好你好你好\n\n")));//这里能否运行是个问号
     vTaskDelay(100);
+    }
 }

ad820027a064608166360e6f4e20e03bc727bbb6
Scaxlibur <scaxlibur@gmail.com>
2024-11-01 21:20:34 +0800

    注册驱动前检查，避免重复注册报错

--- main/UARTtrans.cpp
+++ main/UARTtrans.cpp
@@ -33,7 +33,9 @@ void init_uart2id(void)            //配置连接到指纹识别器的UART
 {
     ESP_ERROR_CHECK(uart_param_config(UART_NUM_ID, &uart2id_config));
     ESP_ERROR_CHECK(uart_set_pin(UART_NUM_ID, UART_NUM_ID_TX, UART_NUM_ID_RX, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
-    ESP_ERROR_CHECK(uart_driver_install(UART_NUM_ID, RX_BUF_SIZE * 2, 0, 0, NULL, 0));
+    if(uart_is_driver_installed(UART_NUM_ID)){//注册驱动前检查，避免重复注册报错
+        ESP_ERROR_CHECK(uart_driver_install(UART_NUM_ID, RX_BUF_SIZE * 2, 0, 0, NULL, 0));
+    };
 }
 
 void init_uart2screen(void)         //配置连接到串口屏的UART

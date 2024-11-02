a27aa02bc641bc22b08136b3c03c681b837d858b
WuLaLaLaZJ <2474380008@qq.com>
2024-11-02 10:18:53 +0800

    闪烁两个LED

--- user/main.c
+++ user/main.c
@@ -8,6 +8,7 @@ int main(void)
 	GPIO_InitTypeDef GPIO_InitStructure;//定义结构体
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP ;//GPIO模式，推挽输出
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;//GPIO的0号引脚
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;//输出速度
 	GPIO_Init(GPIOA,&GPIO_InitStructure);
 	//第一个参数是指初始化GPIOA端口，第二个指初始化GPIO_InitStructure，以便用来定义
@@ -22,9 +23,11 @@ int main(void)
 
 	while(1)
 	{
-		GPIO_WriteBit(GPIOA,GPIO_Pin_0,Bit_SET);		
+		GPIO_WriteBit(GPIOA,GPIO_Pin_0,Bit_SET);
+		GPIO_WriteBit(GPIOA,GPIO_Pin_1,Bit_SET);
 		Delay_ms(500);
 		GPIO_WriteBit(GPIOA,GPIO_Pin_0,Bit_RESET);
+		GPIO_WriteBit(GPIOA,GPIO_Pin_1,Bit_RESET);
 		Delay_ms(500);
 		
 	}
